a3c3f068f41970bb064042c4cd8305f341bf4154
Seongmo Jeon <oeeen3@gmail.com>
2019-12-12 20:43:55 +0900

    Initial Commit

---
+++ src/test/java/dev/smjeon/kakaopay/KakaopayApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.smjeon.kakaopay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KakaopayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
