22d089e85d06445750dcba1da35d64dbc856c33d
Seongmo Jeon <oeeen3@gmail.com>
2019-12-14 17:43:58 +0900

    fix: Database 에 월 잘못 들어가던 버그 수정

---
+++ src/main/java/dev/smjeon/kakaopay/MonthConverter.java
@@ -0,0 +1,19 @@
+package dev.smjeon.kakaopay;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+import java.time.Month;
+
+@Converter(autoApply = true)
+public class MonthConverter implements AttributeConverter<Month, Integer> {
+
+    @Override
+    public Integer convertToDatabaseColumn(Month attribute) {
+        return attribute.getValue();
+    }
+
+    @Override
+    public Month convertToEntityAttribute(Integer dbData) {
+        return Month.of(dbData);
+    }
+}
